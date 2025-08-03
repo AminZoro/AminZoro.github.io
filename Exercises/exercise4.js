eff0091de7877ae5ec03e77b4ceae02b52de6eba
AminZazaa <Aemilio.hernandez19@gmail.com>
2025-08-03 21:15:17 +1000

    labK

---
+++ Exercises/exercise4.js
@@ -0,0 +1,32 @@
+function sayHello() {
+  console.log("wicked");
+}
+
+
+function sum(a, b) {
+  console.log(a + b);
+ 
+}
+
+function returnSum(a, b) {
+  return a + b;
+}
+function minus(a, b) {
+  console.log(a - b);
+  return a -b;
+}
+function times(a, b) {
+  console.log(a * b);
+  return a*b;
+}
+function divide(a, b) {
+  console.log(a / b);
+  return a/b;
+}
+console.log(sum(1,5));
+
+function hiToSomeone(name){
+  console.log('Hello' + name);
+}
+hiToSomeone("Bob");
+
---
+++ Exercises/exercise5.js
@@ -0,0 +1,6 @@
+const alist = [1, 6, 3, 4, 10];
+
+const temp=
+
+alist[1]=5;
+alist[4]=2;
